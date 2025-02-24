9f54caa0165e90acc575e291cbf8a81ef33e1089
Abhinav Randive <arandive27@wooster.edu>
2025-02-24 13:44:28 -0500

    m

--- URLtest.py
+++ URLtest.py
@@ -3,8 +3,8 @@ import pytest
 from httpsCheck import check_and_shorten_url
 
 def test_https_url():
-    url = "https://www.example.com/path"
-    expected = "https://example.com"
+    url = "https://chatgpt.com/c/67bcbbb3-6ab0-800c-892f-73195ee0cae5"
+    expected = "https://chatgpt.com"
     result = check_and_shorten_url(url)
     assert result == expected
 
@@ -15,14 +15,14 @@ def test_http_url():
     assert result == expected
 
 def test_no_www():
-    url = "https://example.com/path"
-    expected = "https://example.com"
+    url = "https://reddit.com/r/webdev/comments/10k3bhq/why_did_we_need_www_for_urls_and_why_dont_we_need/.com/path"
+    expected = "https://reddit.com"
     result = check_and_shorten_url(url)
     assert result == expected
 
 def test_no_path():
-    url = "https://www.example.com"
-    expected = "https://example.com"
+    url = "https://www.youtube.com"
+    expected = "https://youtube.com"
     result = check_and_shorten_url(url)
     assert result == expected
 
