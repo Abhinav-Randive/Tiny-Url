13d95f3c8f168cd335b2d4dcfc2637827fbddf99
MilesFike <156404000+MilesFike@users.noreply.github.com>
2025-02-24 13:10:50 -0500

    Add files via upload
    UPDATED URL SHORTENER AND VALID URL IDENTIFIER

--- httpsCheck.py
+++ httpsCheck.py
@@ -1,29 +1,40 @@
 url = str(input("Give me a URL: "))
 marker = 0
 begin = ""
-
+valid0 = False
+valid1 = False
 #Removes the https://
 if url.startswith("https://"):
     url = url[8:]
     begin = "https://"
+    valid0 = True
 elif url.startswith("http://"):
     url = url[7:]
     begin = "http://"
-print(url)
+    valid0 = True
+
 #Removes the www. if it exists
 if url.startswith("www."):
     url = url[4:]
-print(url)
 
 #Finds the main body of the url discluding all after "/""
 for i in range(len(url)):
     if url[i] == "/":
         marker = i
         break
-print(marker)
 body = url[0:marker + 5]
+
+for i in range (len(body)):
+    if body[i] == ".":
+        valid1 = True
+        break
 
 #compiles and prints shortened url
 newUrl = begin + body
-print(newUrl)
 
+
+if(valid0 == True and valid1 == True):
+    print("URL is Valid")
+else:
+    print("URL is Invalid")
+print("Shortened URL: " + newUrl)
