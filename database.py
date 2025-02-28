a81de1403ca3c535df2387dc76bbf15196ac69cd
celery-sickles <elleryahn@gmail.com>
2025-02-28 13:47:55 -0500

    database and verification

--- database.py
+++ database.py
@@ -7,31 +7,45 @@ filename = 'URL Database.csv'
 header = ['Number', 'URL', 'Short URL']
 
 with open(filename, 'w') as file:
-    writer = csv.writer(file)
-    writer.writerow(header)
+    with open(filename, 'a') as file:
+        readWriter = csv.writer(file)
+        readWriter.writerow(header)
+    more = input('Do you want to add more URLs? (y/n): ')
+    if more.lower() == 'y':
+        count = 1
+        while True:
+            url = input('Enter URL: ')
+            short_url = check_and_shorten_url(url)  # Generate the short URL using the function
 
-more = input('Do you want to add more URLs? (y/n): ')
-if more.lower() == 'y':
-    count = 1
-    while True:
-        url = input('Enter URL: ')
-        short_url = check_and_shorten_url(url)  # Generate the short URL using the function
+            with open(filename, 'a') as file:
+                readWriter = csv.writer(file)
 
-        with open(filename, 'a') as file:
-            writer = csv.writer(file)
+                # Make sure the same long URL is not added twice
+                if not noDuplication(filename, url):
+                    readWriter.writerow([count, url, short_url])  # Write the URL and short URL to the CSV file
+                    count += 1
+                    print("Adding the URL.")
+                else:
+                    print("This URL has already been added.")
+            
+            #testing reading from url database
+            
+            more = input('Do you want to add more URLs? (y/n): ')
+            if more.lower() != 'y':
+                break
+    else:
+        print('No URLs added to database.')
+    
+    #with open(filename, 'r') as file:
+    #    reader = csv.reader(file)
+    #    for row in reader:
+    #        print("Testing",row)
+    print('URLs added to database.')
 
-            # Make sure the same long URL is not added twice
-            if not noDuplication(filename, url):
-                writer.writerow([count, url, short_url])  # Write the URL and short URL to the CSV file
-                count += 1
-                print("Adding the URL.")
-            else:
-                print("This URL has already been added.")
-
-        more = input('Do you want to add more URLs? (y/n): ')
-        if more.lower() != 'y':
-            break
-else:
-    print('No URLs added to database.')
-
-print('URLs added to database.')
+    #Problems:
+    #the first URL gets cut off because it replaces the header
+    #the verification only checks the first row????
+#with open(filename, 'r') as file:
+#        reader = csv.reader(file)
+#        for row in reader:
+#            print("Testing",row)
--- verification.py
+++ verification.py
@@ -1,6 +1,5 @@
 # Make sure there are no duplications in the long URL
 import csv
-#import pandas
 import os
 
 def noDuplication(fileName, newURL):
@@ -8,29 +7,28 @@ def noDuplication(fileName, newURL):
     with open(fileName, 'r') as file:
         # creating a csv reader object
         csvreader = csv.reader(file)
-        #print("csvreader made")
-        #df = pandas.read_csv(fileName)
 
-        if os.stat(fileName).st_size != 0:
-        # this doesn't get read because there's no rows in csvreader
-            for row in csvreader: 
-                print(row)
-                #print(newURL)
-                if row[1] == newURL:
-                        print(row[1])
-                        print("The file exists")
-                        return True
-                else:
-                    print("The file does not exist; can be added")
-                    return False
+        #if os.stat(fileName).st_size != 0:
+
+        #only reads the first row in csvreader???        
+        for row in csvreader: 
+            print("testing in function",row)
+            #print(newURL)
+            if row[1] == newURL:
+                    print(row[1])
+                    print("The file exists")
+                    return True
+            else:
+                print("The file does not exist; can be added")
+                return False
         # always reads the file as empty
-        elif os.stat(fileName).st_size == 0:
-            print("Empty")
-            return False
-        else:
-            print("Something else went wrong")
+        #elif os.stat(fileName).st_size == 0:
+        #    print("Empty")
+        #    return False
+        #else:
+        #    print("Something else went wrong")
 
-noDuplication("URL Database.csv", "https://moodle-2425.wooster.edu/mod/assign/view.php?id=52597")
+#noDuplication("URL Database.csv", "https://moodle-2425.wooster.edu/mod/assign/view.php?id=52597")
     #except FileNotFoundError:
     #    print("File not found")
     
