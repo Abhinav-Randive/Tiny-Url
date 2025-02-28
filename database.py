dbbf1f36a529a1abcf48a530f7ab23ea0ce66b87
celery-sickles <elleryahn@gmail.com>
2025-02-28 13:46:55 -0500

    verification and database

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import csv
 from httpsCheck import check_and_shorten_url  # Import the function from the other module
+from verification import noDuplication
 
 filename = 'URL Database.csv'
 
@@ -9,19 +10,28 @@ with open(filename, 'w') as file:
     writer = csv.writer(file)
     writer.writerow(header)
 
-    more = input('Do you want to add more URLs? (y/n): ')
-    if more.lower() == 'y':
-        count = 1
-        while True:
-            url = input('Enter URL: ')
-            short_url = check_and_shorten_url(url)  # Generate the short URL using the function
+more = input('Do you want to add more URLs? (y/n): ')
+if more.lower() == 'y':
+    count = 1
+    while True:
+        url = input('Enter URL: ')
+        short_url = check_and_shorten_url(url)  # Generate the short URL using the function
 
-            writer.writerow([count, url, short_url])  # Write the URL and short URL to the CSV file
-            count += 1
-            more = input('Do you want to add more URLs? (y/n): ')
-            if more.lower() != 'y':
-                break
-    else:
-        print('No URLs added to database.')
+        with open(filename, 'a') as file:
+            writer = csv.writer(file)
+
+            # Make sure the same long URL is not added twice
+            if not noDuplication(filename, url):
+                writer.writerow([count, url, short_url])  # Write the URL and short URL to the CSV file
+                count += 1
+                print("Adding the URL.")
+            else:
+                print("This URL has already been added.")
+
+        more = input('Do you want to add more URLs? (y/n): ')
+        if more.lower() != 'y':
+            break
+else:
+    print('No URLs added to database.')
 
 print('URLs added to database.')
